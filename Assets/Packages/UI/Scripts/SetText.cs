d1ef5c9aaecdb950f3a006f9b96a417ef63b315b
Thiago-Morais <thiolimor@gmail.com>
2021-02-01 00:35:05 -0300

    Added Remap to setText

--- Assets/Packages/UI/Scripts/SetText.cs
+++ Assets/Packages/UI/Scripts/SetText.cs
@@ -5,18 +5,20 @@ using UnityEngine;
 public class SetText : MonoBehaviour, IInitializable
 {
     public TextMeshProUGUI text;
-
+    [Header("Float")]
+    public bool roundToInt;
+    public RemapHandler floatRemapper;
     public IInitializable Initialized(Transform parent = null)
     {
         transform.SetParent(parent);
         return this;
     }
-    void Awake()
-    {
-
-    }
     public void SetFloat(float value)
     {
-        text.text = value.ToString("F2");
+        float remapped = floatRemapper.Remap(value);
+
+        if (roundToInt) remapped = Mathf.RoundToInt(remapped);
+
+        text.text = remapped.ToString("F2");
     }
 }
